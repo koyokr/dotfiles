f5b234f44a4afca81dcc0749be360d02b9a8215f
Younghun Ko <koyo@koyo.kr>
2021-06-01 16:02:34 +0900

    Update ll alias in powershell

--- Windows/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,6 +2,6 @@ Invoke-Expression (&starship init powershell)
 Set-Alias ls lsd
 function l { lsd @args }
 function la { lsd -a @args }
-function ll { lsd -l --date +'%Y-%m-%d %H:%M' @args }
+function ll { lsd -l --blocks 'permission,size,date,name' --date +'%Y-%m-%d %H:%M' @args }
 function lla { ll -a @args }
 function lt { lsd --tree --depth @args }

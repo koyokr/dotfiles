6bdd554bf3ece5bff0f68d15f195a623825af1c2
Younghun Ko <koyo@koyo.kr>
2023-02-09 23:16:27 +0900

    2023-02-09

--- Windows/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ Windows/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,9 +1,11 @@
-Invoke-Expression (&starship init powershell)
-
 Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
 Set-PSReadLineOption -BellStyle None -PredictionSource History
 
-function l { lsd @args }
-function la { lsd -a @args }
-function ll { lsd -l --blocks 'permission,size,date,name' --date '+%Y-%m-%d %H:%M' @args }
+function Invoke-Starship-PreCommand { $Host.UI.RawUI.WindowTitle = $PWD.ToString().Replace($HOME, '~').Replace('\', '/') }
+Invoke-Expression (&starship init powershell)
+
+Set-Alias -Name l -Value lsd
+function la { l -a @args }
+function ll { l -l --blocks 'permission,size,date,name' --date '+%Y-%m-%d %H:%M' @args }
 function lla { ll -a @args }
+function lt { lsd --tree -depth @args }

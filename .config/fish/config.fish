7ddc5ce4e972248c668ef28a7563f4e64d498206
Younghun Ko <koyo@koyo.kr>
2024-03-01 13:49:38 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,10 +2,8 @@ if status is-interactive
     starship init fish | source
     set -gx GPG_TTY (tty)
 
-    alias ls  'lsd'
-    alias l   'lsd'
-    alias la  'lsd -a'
-    alias ll  'lsd -l --date +"%Y-%m-%d %H:%M"'
-    alias lla 'll -a'
-    alias lt  'lsd --tree --depth'
+    alias ls 'lsd'
+    alias l  'lsd'
+    alias la 'lsd -a'
+    alias ll 'lsd -al --date +"%Y-%m-%d %H:%M"'
 end

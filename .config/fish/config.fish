087eee327be0759b213cffc4c9661d6c884cfb06
Younghun Ko <koyo@koyo.kr>
2024-03-22 17:49:42 +0900

    Update config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,4 +6,7 @@ if status is-interactive
     alias l  'lsd'
     alias la 'lsd -a'
     alias ll 'lsd -al --date +"%Y-%m-%d %H:%M"'
+
+    alias cosh  'gh copilot suggest -t shell'
+    alias cogit 'gh copilot suggest -t git'
 end

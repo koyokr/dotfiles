c1216e2ef701a3e26773652160cccb0ed839a326
Younghun Ko <koyo@koyo.kr>
2025-01-03 15:32:59 +0900

    Update bootstrap.ps1

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,27 +1,12 @@
-# Set execution policy to allow script execution
-Set-ExecutionPolicy -ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
-
-# Install packages using winget
-$wingetPackages = @(
-    'Microsoft.VCRedist.2015+.x64',
-    'Microsoft.VCRedist.2015+.x86',
-    'Bandisoft.BandiView',
-    'CondaForge.Miniforge3',
-    'DigitalScholar.Zotero',
-    'Microsoft.PowerShell',
-    'Microsoft.PowerToys',
-    'Microsoft.VisualStudioCode',
-    'Obsidian.Obsidian'
-)
-
-Write-Host 'Installing applications using winget...'
-winget install $wingetPackages --accept-source-agreements --accept-package-agreements --no-upgrade --silent
-
 # Install Scoop if not already installed
 if (!(Get-Command scoop -ErrorAction SilentlyContinue)) {
     Write-Host 'Installing Scoop...'
     Invoke-RestMethod -Uri https://get.scoop.sh | Invoke-Expression
 }
+
+# Disable aria2 warning message for faster downloads
+Write-Host 'Configuring Scoop settings...'
+scoop config aria2-warning-enabled false
 
 # Add Scoop buckets
 Write-Host 'Adding Scoop buckets...'
@@ -49,7 +34,6 @@ $scoopPackages = @(
     'lsd',
     'mpv',
     'neovim',
-    'nodejs-lts',
     'openssh',
     'psfzf',
     'python',
@@ -58,7 +42,6 @@ $scoopPackages = @(
     'sd',
     'starship',
     'sysinternals',
-    'temurin-lts-jdk',
     'wget',
     'zoxide'
 )
@@ -75,39 +58,61 @@ Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer
 
 # Configure Snap layouts and multitasking settings
 Write-Host 'Configuring snap layouts and multitasking settings...'
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'EnableSnapAssistFlyout' -Value 0
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'EnableSnapBar' -Value 0
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'SnapAssist' -Value 0
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'DisablePreviewWindow' -Value 1
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'EnableTaskGroups' -Value 1
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'MultiTaskingAltTabFilter' -Value 3
+
+New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Services\UCPD' -Name 'Start' -Value 4 -PropertyType DWORD -Force
+Disable-ScheduledTask -TaskName ‘\Microsoft\Windows\AppxDeploymentClient\UCPD velocity’
 
 # Configure taskbar settings
 Write-Host 'Configuring taskbar settings...'
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Search" -Name "SearchboxTaskbarMode" -Value 1
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarAl" -Value 0
-Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "TaskbarDa" -Value 0
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Search' -Name 'SearchboxTaskbarMode' -Value 1
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'TaskbarAl' -Value 0
+# Disabled due to UCPD driver blocking registry modifications
+# Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'TaskbarDa' -Value 0
 
 # Configure system tray clock (24-hour format with seconds)
 Write-Host 'Configuring system tray clock format...'
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced' -Name 'ShowSecondsInSystemClock' -Value 1
-Set-ItemProperty -Path "HKCU:\Control Panel\International" -Name "sShortTime" -Value "HH:mm"
-Set-ItemProperty -Path "HKCU:\Control Panel\International" -Name "sTimeFormat" -Value "HH:mm:ss"
+Set-ItemProperty -Path 'HKCU:\Control Panel\International' -Name 'sShortTime' -Value 'HH:mm'
+Set-ItemProperty -Path 'HKCU:\Control Panel\International' -Name 'sTimeFormat' -Value 'HH:mm:ss'
 
 # Enable clipboard history
 Write-Host 'Enabling clipboard history...'
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Clipboard' -Name 'EnableClipboardHistory' -Value 1
 
-# Remove MAX_PATH limit
-Write-Host 'Removing MAX_PATH length limit...'
-gsudo New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1 -PropertyType DWORD -Force
+gsudo {
+    # Remove MAX_PATH limit
+    Write-Host 'Removing MAX_PATH length limit...'
+    New-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem' -Name 'LongPathsEnabled' -Value 1 -PropertyType DWORD -Force
+    
+    # Disable search box suggestions
+    Write-Host 'Disabling search box suggestions...'
+    New-Item -Path 'HKCU:\Software\Policies\Microsoft\Windows\Explorer' -Force | Out-Null
+    Set-ItemProperty -Path 'HKCU:\Software\Policies\Microsoft\Windows\Explorer' -Name 'DisableSearchBoxSuggestions' -Value 1 -Type DWord
+    
+    # Remove Windows Features
+    Write-Host 'Removing Windows Features...'
+    Disable-WindowsOptionalFeature -Online -FeatureName 'SMB1Protocol' -NoRestart
+    Disable-WindowsOptionalFeature -Online -FeatureName 'MicrosoftWindowsPowerShellV2' -NoRestart
 
-# Disable search box suggestions
-Write-Host 'Disabling search box suggestions...'
-gsudo New-Item -Path 'HKCU:\Software\Policies\Microsoft\Windows\Explorer' -Force | Out-Null
-gsudo Set-ItemProperty -Path 'HKCU:\Software\Policies\Microsoft\Windows\Explorer' -Name 'DisableSearchBoxSuggestions' -Value 1 -Type DWord
-
-# Remove Windows Features
-Write-Host 'Removing Windows Features...'
-gsudo Disable-WindowsOptionalFeature -Online -FeatureName "SMB1Protocol" -NoRestart
-gsudo Disable-WindowsOptionalFeature -Online -FeatureName "MicrosoftWindowsPowerShellV2" -NoRestart
+    # Install packages using winget
+    $wingetPackages = @(
+        'Microsoft.VCRedist.2015+.x64',
+        'Microsoft.VCRedist.2015+.x86',
+        'Bandisoft.BandiView',
+        'CondaForge.Miniforge3',
+        'Microsoft.PowerShell',
+        'Microsoft.PowerToys',
+        'Microsoft.VisualStudioCode',
+        'Obsidian.Obsidian'
+    )
+    
+    Write-Host 'Installing applications using winget...'
+    winget install $wingetPackages --accept-source-agreements --accept-package-agreements --no-upgrade --silent
+}
 
 # Restart Explorer to apply changes
 Stop-Process -Name explorer -Force

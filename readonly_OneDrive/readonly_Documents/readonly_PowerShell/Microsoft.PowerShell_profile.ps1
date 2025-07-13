3e97ca6e5c372ea14820c960944517f80a685b3b
Younghun Ko <koyo@koyo.kr>
2025-07-14 01:19:36 +0900

    Sync latest config changes

--- readonly_OneDrive/readonly_Documents/readonly_PowerShell/Microsoft.PowerShell_profile.ps1
+++ readonly_OneDrive/readonly_Documents/readonly_PowerShell/Microsoft.PowerShell_profile.ps1
@@ -2,20 +2,11 @@
 function Invoke-Starship-PreCommand { $Host.UI.RawUI.WindowTitle = $PWD.ToString().Replace($HOME, '~').Replace('\', '/') }
 Invoke-Expression (&starship init powershell)
 
-# scoop
-Invoke-Expression (&scoop-search --hook)
-
 # zoxide
 Invoke-Expression (&{zoxide init --hook 'pwd' powershell | Out-String})
 
 # psfzf
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
 
-# lsd
-function l  { lsd @args }
-function la { lsd -a @args }
-function ll { lsd -la --blocks 'permission,size,date,name' --date '+%Y-%m-%d %H:%M' @args }
-
-# gh copilot
-$GH_COPILOT_PROFILE = Join-Path -Path $(Split-Path -Path $PROFILE -Parent) -ChildPath "gh-copilot.ps1"
-. $GH_COPILOT_PROFILE
+# scoop
+Invoke-Expression (&scoop-search --hook)

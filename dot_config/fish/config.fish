c5fcd4108bd9f0ff32a04c91744f1d8c55fb1848
Younghun Ko <koyo@koyo.kr>
2025-07-14 14:09:37 +0900

    Update config.fish

--- dot_config/fish/config.fish
+++ dot_config/fish/config.fish
@@ -1,12 +1,10 @@
 if status is-interactive
     starship init fish | source
+    zoxide init fish | source
     set -gx GPG_TTY (tty)
 
-    alias ls 'lsd'
-    alias l  'lsd'
-    alias la 'lsd -a'
-    alias ll 'lsd -al --date +"%Y-%m-%d %H:%M"'
-
-    alias ghcs  'gh copilot suggest -t shell'
-    alias ghce 'gh copilot suggest -t git'
+    alias ls 'ls --color=auto'
+    alias ll 'ls -alF'
+    alias la 'ls -A'
+    alias l 'ls -CF'
 end

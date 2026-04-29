4005d9ad4063154f1ca702ecdd108394ead5c56d
Younghun Ko <koyo@koyo.kr>
2026-04-29 12:04:38 +0900

    Update PowerShell profile with zoxide and psfzf changes

--- home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -3,10 +3,10 @@ function Invoke-Starship-PreCommand { $Host.UI.RawUI.WindowTitle = $PWD.ToString
 Invoke-Expression (&starship init powershell)
 
 # zoxide
-Invoke-Expression (&{zoxide init --hook 'pwd' powershell | Out-String})
+Invoke-Expression (&{zoxide init --cmd cd powershell | Out-String})
 
 # psfzf
-Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+f' -PSReadlineChordReverseHistory 'Ctrl+r'
+Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
 
 # scoop
 Invoke-Expression (&scoop-search --hook)
